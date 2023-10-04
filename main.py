5d33122a0199baa075bca4c9dca240636b2d26f5
alireza-urmia <alirezamohamad5566@gmail.com>
2023-10-04 16:44:06 +0330

    try & except for successfully  write result to txt file

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ playlist = Playlist('https://youtube.com/playlist?list=PLLRM7ROnmA9H_8wqcxoKTMU5
 
 
 # Save episodes links of playlist to text file
-with open('episodes_urls.txt', 'w') as f:
-    f.write('number of videos exist on playlist: ' + str(len(playlist.video_urls)) + '\n' + '\n')
-    for video_url in playlist.video_urls:
-        f.write(video_url + '\n')
+try:
+    with open('episodes_urls.txt', 'w') as f:
+        f.write('number of videos exist on playlist: ' + str(len(playlist.video_urls)) + '\n' + '\n')
+        for video_url in playlist.video_urls:
+            f.write(video_url + '\n')
+    print("Episodes links saved to 'episodes_urls.txt' successfully!")
+except Exception as e:
+    print("Error:", str(e))
